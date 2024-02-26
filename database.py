fd0d26a2ec9cd9aa6700abe1db575114b37d385a
marcoseugenioln <marcos.eugenioln@gmail.com>
2024-02-26 19:36:06 -0300

    set default language to english

--- database.py
+++ database.py
@@ -21,8 +21,8 @@ class Database():
 
     def user_exists(self, email: str, password: str) -> bool:
 
-        self.query.execute(f"SELECT * FROM usuario WHERE email == '{email}' AND password == '{password}'")
-        logger.info(f"SELECT * FROM usuario WHERE email == '{email}' AND password == '{password}'")
+        self.query.execute(f"SELECT * FROM user WHERE email == '{email}' AND password == '{password}'")
+        logger.info(f"SELECT * FROM user WHERE email == '{email}' AND password == '{password}'")
 
         account = self.query.fetchone()
 
@@ -32,14 +32,14 @@ class Database():
         return True
     
     def get_user_id(self, email: str, password: str):
-        logger.info(f"SELECT id FROM usuario WHERE email == '{email}' AND password == '{password}';")
-        self.query.execute(f"SELECT id FROM usuario WHERE email == '{email}' AND password == '{password}';")
+        logger.info(f"SELECT id FROM user WHERE email == '{email}' AND password == '{password}';")
+        self.query.execute(f"SELECT id FROM user WHERE email == '{email}' AND password == '{password}';")
         user_id = self.query.fetchone()[0]   
         return user_id
 
     def get_admin(self, user_id):
-        logger.info(f"SELECT is_admin FROM usuario WHERE id == {user_id};")
-        self.query.execute(f"SELECT is_admin FROM usuario WHERE id == {user_id};")
+        logger.info(f"SELECT is_admin FROM user WHERE id == {user_id};")
+        self.query.execute(f"SELECT is_admin FROM user WHERE id == {user_id};")
         is_admin = self.query.fetchone()
 
         if int(is_admin[0]) == 0:
@@ -48,45 +48,45 @@ class Database():
         return True
     
     def insert_user(self, email: str, password: str, is_admin: str) -> bool:        
-        self.query.execute(f"INSERT OR IGNORE INTO usuario(email, password, is_admin) values ('{email}', '{password}', {is_admin});")
-        logger.info(f"INSERT OR IGNORE INTO usuario(email, password, is_admin) values ('{email}', '{password}', {is_admin});")
+        self.query.execute(f"INSERT OR IGNORE INTO user(email, password, is_admin) values ('{email}', '{password}', {is_admin});")
+        logger.info(f"INSERT OR IGNORE INTO user(email, password, is_admin) values ('{email}', '{password}', {is_admin});")
         self.connection.commit()
         return True
     
     def get_user_email(self, user_id: int) -> str:
-        self.query.execute(f"SELECT email FROM usuario WHERE id == {user_id}")
-        logger.info(f"SELECT email FROM usuario WHERE id == {user_id}")
+        self.query.execute(f"SELECT email FROM user WHERE id == {user_id}")
+        logger.info(f"SELECT email FROM user WHERE id == {user_id}")
         email = self.query.fetchone()[0]
         return email
 
     def get_user_password(self, user_id: int) -> str:
-        self.query.execute(f"SELECT password FROM usuario WHERE id == {user_id}")
-        logger.info(f"SELECT password FROM usuario WHERE id == {user_id}")
+        self.query.execute(f"SELECT password FROM user WHERE id == {user_id}")
+        logger.info(f"SELECT password FROM user WHERE id == {user_id}")
         password = self.query.fetchone()[0]
         return password
     
     def get_users(self):
-        self.query.execute(f"SELECT id, email, password FROM usuario")
-        logger.info(f"SELECT id, email, password FROM usuario")
+        self.query.execute(f"SELECT id, email, password FROM user")
+        logger.info(f"SELECT id, email, password FROM user")
         users = self.query.fetchall()        
         return users
     
     def alter_password(self, user_id, password):
-        self.query.execute(f"UPDATE usuario SET password = '{password}' WHERE id == {user_id}")
-        logger.info(f"UPDATE usuario SET password = '{password}' WHERE id == {user_id}")
+        self.query.execute(f"UPDATE user SET password = '{password}' WHERE id == {user_id}")
+        logger.info(f"UPDATE user SET password = '{password}' WHERE id == {user_id}")
         self.connection.commit()
 
     def alter_email(self, user_id, email):
-        self.query.execute(f"UPDATE usuario SET email = '{email}' WHERE id == {user_id}")
-        logger.info(f"UPDATE usuario SET email = '{email}' WHERE id == {user_id}")
+        self.query.execute(f"UPDATE user SET email = '{email}' WHERE id == {user_id}")
+        logger.info(f"UPDATE user SET email = '{email}' WHERE id == {user_id}")
         self.connection.commit()
 
     def delete_user(self, id):
-        self.query.execute(f"DELETE FROM usuario WHERE id == {id};")
-        logger.info(f"DELETE FROM usuario WHERE id == {id};")
+        self.query.execute(f"DELETE FROM user WHERE id == {id};")
+        logger.info(f"DELETE FROM user WHERE id == {id};")
         self.connection.commit()
 
     def update_user(self, user_id, email, password, is_admin):
-        logger.info(f"UPDATE usuario SET email = '{email}', password = '{password}', is_admin = {is_admin} WHERE id == {user_id};")
-        self.query.execute(f"UPDATE usuario SET email = '{email}', password = '{password}', is_admin = {is_admin} WHERE id == {user_id};")
+        logger.info(f"UPDATE user SET email = '{email}', password = '{password}', is_admin = {is_admin} WHERE id == {user_id};")
+        self.query.execute(f"UPDATE user SET email = '{email}', password = '{password}', is_admin = {is_admin} WHERE id == {user_id};")
         self.connection.commit()
